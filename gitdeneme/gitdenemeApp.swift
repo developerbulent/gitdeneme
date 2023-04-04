1489234586351dd7d00bd26d2dbf8da68e0ac463
developerbulent <bulentmailforwork@gmail.com>
2023-04-04 23:20:15 +0300

    Initial Commit

---
+++ gitdeneme/gitdenemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitdenemeApp.swift
+//  gitdeneme
+//
+//  Created by Bulent on 4.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct gitdenemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
